3d1e38db21d75fb081543f614600be70795ba130
christopherlovell <chrisl1991@hotmail.co.uk>
2025-06-08 09:24:25 +0100

    update grids

--- grids.py
+++ grids.py
@@ -1,7 +1,5 @@
-import torch
 import h5py
 from scipy.stats import qmc
-from torch.utils.data import Dataset
 import jax.numpy as jnp
 import numpy as np
 
@@ -10,42 +8,6 @@ from synthesizer.particle import Stars
 from synthesizer.emission_models import IncidentEmission
 from unyt import Msun, yr, angstrom
 
-class SpectralDataset(Dataset):
-    def __init__(self, h5_path):
-        with h5py.File(h5_path, 'r') as f:
-            self.spectra = torch.tensor(f['spectra/incident'][:], dtype=torch.float32)
-            self.wavelength = torch.tensor(f['spectra/wavelength'][:], dtype=torch.float32)
-            self.ages = torch.tensor(f['axes/ages'][:], dtype=torch.float32)
-            self.metallicities = torch.tensor(f['axes/metallicities'][:], dtype=torch.float32)
-
-        # Filter spectra and wavelength
-        mask = (self.wavelength > 1000) & (self.wavelength < 10000)
-        self.spectra = self.spectra[:, :, mask]
-        self.wavelength = self.wavelength[mask]
-
-        # Get dimensions
-        self.n_age, self.n_met, self.n_wavelength = self.spectra.shape
-        
-        # Normalize parameters
-        self.ages = (self.ages - self.ages.mean()) / self.ages.std()
-        self.metallicities = (self.metallicities - self.metallicities.mean()) / self.metallicities.std()
-        
-        # Create all combinations of parameters
-        self.conditions = torch.stack(torch.meshgrid(self.ages, self.metallicities, indexing='ij')).reshape(2, -1).T
-
-        # Reshape spectra to match conditions
-        self.spectra = self.spectra.reshape(-1, self.n_wavelength)
-
-        # Log and normalize spectra
-        self.spectra = torch.log10(self.spectra)
-        self.spectra = (self.spectra - self.spectra.mean(dim=1, keepdim=True)) / self.spectra.std(dim=1, keepdim=True)
-    
-    def __len__(self):
-        return len(self.conditions)
-    
-    def __getitem__(self, idx):
-        return self.conditions[idx], self.spectra[idx]
-    
 
 class SpectralDatasetJAX:
     def __init__(
@@ -141,7 +103,7 @@ def LHGridSpectra(grid_dir, grid_name, num_samples=1000):
     ages = samples[:, 0]
     metallicities = samples[:, 1]
 
-    return spectra, wavelength, initial_masses, ages, metallicities
+    return spectra, wavelength, ages, metallicities
 
 
 class SpectralDatasetSynthesizer:
@@ -157,7 +119,7 @@ class SpectralDatasetSynthesizer:
             # self.n_age = parent_dataset.n_age
             # self.n_met = parent_dataset.n_met
         else:
-            self.spectra, self.wavelength, self.initial_masses, self.ages, self.metallicities = LHGridSpectra(grid_dir, grid_name, num_samples)
+            self.spectra, self.wavelength, self.ages, self.metallicities = LHGridSpectra(grid_dir, grid_name, num_samples)
             
             self.n_wavelength = self.spectra.shape[1]
 
@@ -190,11 +152,10 @@ class SpectralDatasetSynthesizer:
 
 
 if __name__ == '__main__':
-    spectra, conditions, wavelength, initial_masses, ages, metallicities = LHGridSpectra()
+    spectra, conditions, wavelength, ages, metallicities = LHGridSpectra()
 
     print(spectra.shape)
     print(conditions.shape)
     print(wavelength.shape)
-    print(initial_masses.shape)
     print(ages.shape)
     print(metallicities.shape)
